b86a796980c60fccd7f9118530ecdfd2168a86a8
Giovanni Harting <539@idlegandalf.com>
2016-07-20 11:59:06 +0200

    fix for concurrentmodex, again :P

--- src/main/java/com/sosnitzka/taiga/util/TickTaskHandler.java
+++ src/main/java/com/sosnitzka/taiga/util/TickTaskHandler.java
@@ -3,13 +3,15 @@ package com.sosnitzka.taiga.util;
 
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.relauncher.Side;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class TickTaskHandler {
-    private List<TickTask> tickTasks = new ArrayList<TickTask>();
+    private List<TickTask> clientTickTasks = new ArrayList<TickTask>();
+    private List<TickTask> serverTickTasks = new ArrayList<TickTask>();
 
     private TickTaskHandler() {
     }
@@ -19,20 +21,31 @@ public class TickTaskHandler {
     }
 
     @SubscribeEvent
-    public void onTick(TickEvent e) {
-        for (Iterator<TickTask> iterator = tickTasks.iterator(); iterator.hasNext(); ) {
-            TickTask task = iterator.next();
+    public synchronized void onTick(TickEvent e) {
+        for (Iterator<TickTask> clientIterator = clientTickTasks.iterator(); clientIterator.hasNext(); ) {
+            TickTask task = clientIterator.next();
 
-            if (e.side == task.getSide() || task.getSide() == null) {
-                if (task.tickRun()) {
-                    iterator.remove();
-                }
+            if (task.tickRun()) {
+                clientIterator.remove();
+            }
+        }
+
+        for (Iterator<TickTask> serverIterator = serverTickTasks.iterator(); serverIterator.hasNext(); ) {
+            TickTask task = serverIterator.next();
+
+            if (task.tickRun()) {
+                serverIterator.remove();
             }
         }
     }
 
     public void addTask(TickTask tickTask) {
-        tickTasks.add(tickTask);
+        if (tickTask.getSide() == Side.CLIENT || tickTask.getSide() == null)
+            clientTickTasks.add(tickTask);
+
+        if (tickTask.getSide() == Side.SERVER || tickTask.getSide() == null)
+            serverTickTasks.add(tickTask);
+
     }
 
     private static final class InstanceHolder {
