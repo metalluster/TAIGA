149edf82cec1966eb6763ba2b388bc7eff54a8c0
Robert Sosnitzka <robert.sosnitzka@gmail.com>
2016-07-20 11:09:06 +0200

    Cascade for trait cascade added. Needs to be fixed.

--- src/main/java/com/sosnitzka/taiga/traits/TraitCascade.java
+++ src/main/java/com/sosnitzka/taiga/traits/TraitCascade.java
@@ -1,13 +1,19 @@
 package com.sosnitzka.taiga.traits;
 
+import com.sosnitzka.taiga.util.TickTask;
+import com.sosnitzka.taiga.util.TickTaskHandler;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
 import slimeknights.tconstruct.library.traits.AbstractTrait;
 import slimeknights.tconstruct.library.utils.ToolHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TraitCascade extends AbstractTrait {
 
@@ -16,13 +22,14 @@ public class TraitCascade extends AbstractTrait {
     }
 
     @Override
-    public void afterBlockBreak(ItemStack tool, World world, IBlockState state, BlockPos pos, EntityLivingBase player, boolean wasEffective) {
+    public void afterBlockBreak(ItemStack tool, final World world, IBlockState state, BlockPos pos, EntityLivingBase player, boolean wasEffective) {
         float f = random.nextFloat();
         if (!world.isRemote && tool.canHarvestBlock(state) && f <= 0.1) {
             double x, y, z, sx, sy, sz;
             sx = x = pos.getX();
             sy = y = pos.getY();
             sz = z = pos.getZ();
+            final List<BlockPos> posliste = new ArrayList<BlockPos>();
             for (int i = random.nextInt((int) (ToolHelper.getCurrentDurability(tool) * 1.5f)); i > 0; i--) { // TODO: limit to 100
                 int r = random.nextInt(3);
                 int d = random.nextBoolean() ? 1 : -1;
@@ -31,7 +38,8 @@ public class TraitCascade extends AbstractTrait {
                 if (r == 2) z += d;
                 BlockPos nextBlock = new BlockPos(x, y, z);
                 if (world.getBlockState(nextBlock).equals(world.getBlockState(pos))) {
-                    world.destroyBlock(nextBlock, true);
+                    //world.destroyBlock(nextBlock, true);
+                    posliste.add(nextBlock);
                     sx = x = nextBlock.getX();
                     sy = y = nextBlock.getY();
                     sz = z = nextBlock.getZ();
@@ -41,6 +49,16 @@ public class TraitCascade extends AbstractTrait {
                     y = sy;
                     z = sz;
                 }
+            }
+
+            for (int i = 0; i < posliste.size(); i++) {
+                final int finalI = i;
+                TickTaskHandler.getInstance().addTask(new TickTask(5, Side.CLIENT, new Runnable() {
+                    @Override
+                    public void run() {
+                        world.destroyBlock(posliste.get(finalI), true);
+                    }
+                }));
             }
         }
     }
